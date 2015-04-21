7d435045680d399796738573d6522f021702b26e
watsokel <watsokel@onid.oregonstate.edu>
2015-04-21 11:56:05 -0700

    Modified the makeRequest function to retrieve only the first page onclick, so that subsequent button presses on the HTML page will trigger requests for subsequent pages.

--- watsokelA3Gist.js
+++ watsokelA3Gist.js
@@ -1,8 +1,9 @@
 (function() {
   var httpRequest;
+  var selectedPages;
+
   document.getElementById('ajaxButton').onclick = function() { 
     var numPages = document.getElementsByName('pages');
-    var selectedPages;
     for(var k=0, len=numPages.length; k<len; k++){
       if(numPages[k].checked){ 
         selectedPages = numPages[k].value;
@@ -12,12 +13,11 @@
     var langs = document.getElementsByName('languages');
     var selectedLangs = [];
     for(var k=0, len=langs.length; k<len; k++){
-      if(languages[k].checked) {
-        selectedLangs.push(languages[k]);
+      if(langs[k].checked) {
+        selectedLangs.push(langs[k].value);
       }
     }
-    debugger;
-    makeRequest('https://api.github.com/gists',selectedPages,selectedLanguages);
+    makeRequest('https://api.github.com/gists',1,selectedLangs);
   }
 
   function makeRequest(url, sPages, sLanguages){
@@ -34,8 +34,9 @@
       return false;
     }
     httpRequest.onreadystatechange = processData;
-    httpRequest.open('GET', 'https://api.github.com/gists', true);
+    httpRequest.open('GET', 'https://api.github.com/gists?page='+sPages+'&per_page='+30, true);
     httpRequest.send(null);
+    }
   }
 
   function processData(){
@@ -43,12 +44,19 @@
       if(httpRequest.readyState===4){
         if(httpRequest.status===200){
           var response = JSON.parse(httpRequest.responseText);
-          console.log(response);
-        }else alert('Problem with the request');
+          displayResults(response);
+        }else console.log('Problem with the request');
       }
     }
     catch(e){
-      alert('Caught Exception: ' + e.description);
+      console.log('Caught Exception: ' + e.description);
     }
   }
+
+  function displayResults(r){
+
+
+  }
+
 })();
+
